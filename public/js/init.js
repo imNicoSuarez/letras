f0aede16367ec087ce2eccbda72da19edda40fb1
gusaaaaa <gusa@neo.com>
2013-10-21 00:17:32 -0200

    Automatically changes font color while typing
    * Turns textarea into a regular div
    * Picks random color when keypress
    
    TODO:
    
    * Automatically scroll down

--- public/js/init.js
+++ public/js/init.js
@@ -1,3 +1,11 @@
 $(function () {
-  $("#theEditor").focus();
+  $("body").keypress(function( event ) {
+    var obj = $("#theEditor"),
+        chr = event.charCode,
+        colors = ["red", "green", "blue", "orange", "pink", "gray", "brown", "black"],
+        i = Math.floor(Math.random() * colors.length);
+
+    obj.append("<span style=\"color: " + colors[i] + ";\">" + String.fromCharCode(chr) + "</span>");
+    event.preventDefault();
+  });
 });
