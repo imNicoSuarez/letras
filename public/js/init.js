b097ae6123931c9067c57b427571700ac84aa5e9
gusaaaaa <gusa@neo.com>
2013-10-21 11:01:20 -0200

    Fades in letters

--- public/js/init.js
+++ public/js/init.js
@@ -8,9 +8,12 @@ $(function () {
   $("body").keypress(function( event ) {
     var chr = event.charCode,
         colors = ["red", "green", "blue", "orange", "pink", "gray", "brown", "black"],
-        i = Math.floor(Math.random() * colors.length);
+        i = Math.floor(Math.random() * colors.length),
+        span = $("<span style=\"color: " + colors[i] + ";\">" + String.fromCharCode(chr) + "</span>").hide();
 
-    $editor.append("<span style=\"color: " + colors[i] + ";\">" + String.fromCharCode(chr) + "</span>");
+
+    $editor.append(span);
+    span.show('slow');
 
     $editor.scrollTop($editor.prop("scrollHeight"));
 
