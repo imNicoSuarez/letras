48e2e74ce2fd6c9a070dea5f57e37e585758d3f8
gusa <gustavoa@gmail.com>
2015-06-29 20:41:20 -0300

    [WIP] Refactors symbol creation

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,44 @@
-$(function () {
+var SymbolFactory = (function() {
+  var colors, symbolElements;
+  colors = ["red", "green", "blue", "orange", "pink", "gray", "brown", "black"];
+  symbolElements = {
+    'A': function() {
+      var symbolContainer, paths, pathLength, maxPathLength = -1;
+      symbolContainer = Snap.parse("<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\" width=\"118.909px\" height=\"155.824px\" viewBox=\"0 0 118.909 155.824\" enable-background=\"new 0 0 118.909 155.824\" xml:space=\"preserve\"><g fill=\"none\" stroke-width=\"6\" stroke=\"" + colors[ Math.floor(Math.random() * colors.length) ] + "\" stroke-linejoin=\"round\" class=\"c-char-a\"><path class=\"c-char-a_first\" fill=\"none\" stroke-miterlimit=\"10\"d=\"M0.467,155.638L59.442,0.5h0.025l58.974,155.146\"/><path class=\"c-char-a_second\" fill=\"none\" stroke-miterlimit=\"10\" d=\"M18.006,109.5h83\"/></g></svg>");
+      paths = symbolContainer.selectAll("path");
+      paths.forEach(function( path ) {
+        pathLength = path.getTotalLength();
+        if ( pathLength > maxPathLength ) maxPathLength = pathLength;
+        path.attr({
+          strokeDasharray: pathLength + " " + pathLength,
+          strokeDashoffset: pathLength
+        });
+      });
+      return {
+        element: function() { return $(symbolContainer.node); },
+        animate: function() {
+          var defChain = $.Deferred().resolve();
+          paths.forEach(function( path ) {
+            defChain = defChain.then(function() {
+              var duration = 300;
+              return $.Deferred(function( dfd ) {
+                path.animate( { strokeDashoffset: "0" }, duration * path.getTotalLength() / maxPathLength, mina.linea, dfd.resolve );
+              }).promise();
+            });
+          });
+        }
+      }
+    }
+  };
+
+  return {
+    create: function( character ) {
+      return symbolElements[ character ]();
+    },
+  };
+})();
+
+$( document ).ready(function () {
 
   var fontHeight = ($(window).height() / 3) + "px",
       editor = $(".js-editor");
@@ -18,38 +58,13 @@ $(function () {
 
   // Symbols can be, e.g. "A", "1", "&nbsp;", etc.
   function appendSymbol( container, symbol ) {
-    var colors = ["red", "green", "blue", "orange", "pink", "gray", "brown", "black"],
-        i = Math.floor(Math.random() * colors.length),
-        symbolContainer;
+    var symbol;
 
-        symbolContainer = $("<span><svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\" width=\"118.909px\" height=\"155.824px\" viewBox=\"0 0 118.909 155.824\" enable-background=\"new 0 0 118.909 155.824\" xml:space=\"preserve\"><g fill=\"none\" stroke-width=\"5\" stroke=\"" + colors[i] + "\" class=\"c-char-a\"><path class=\"c-char-a_first\" fill=\"none\" stroke-miterlimit=\"10\"d=\"M0.467,155.638L59.442,0.5h0.025l58.974,155.146\"/><path class=\"c-char-a_second\" fill=\"none\" stroke-miterlimit=\"10\" d=\"M18.006,109.5h83\"/></g></svg></span>");
+    symbol = SymbolFactory.create( symbol );
+    container.append( symbol.element() );
+    symbol.animate();
 
-    var paths, lengths;
-
-    paths = [ Snap( symbolContainer.find(".c-char-a_first").get(0) ), Snap( symbolContainer.find(".c-char-a_second").get(0) ) ];
-    lengths = [ paths[0].getTotalLength(), paths[1].getTotalLength() ];
-
-    paths[0].attr({
-      strokeDasharray: lengths[0] + " " + lengths[0],
-      strokeDashoffset: lengths[0]
-    });
-
-    paths[1].attr({
-      strokeDasharray: lengths[1] + " " + lengths[1],
-      strokeDashoffset: lengths[1]
-    });
-
-    container.append( symbolContainer );
-
-    paths[0].animate({
-      strokeDashoffset: "0"
-    }, 300, mina.linear, function() {
-      paths[1].animate({
-        strokeDashoffset: "0"
-      }, 100);
-    });
-
-    moveCursorAfter( symbolContainer[0] );
+    moveCursorAfter( symbol.element()[0] );
   }
 
   $("html").keydown( function( event ) {
